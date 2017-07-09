2263e3b62f3b4d2429893d1a7b7f7e59c4e62019
Azzapop <aaron.b.hook@gmail.com>
2017-07-09 13:41:26 +1000

    add route to get job results

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import operator
 import re
 import nltk
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
@@ -24,7 +24,7 @@ from models import *
 def count_and_save_words(url):
     errors = []
     try:
-        r = requests.get(r)
+        r = requests.get(url)
     except:
         errors.append(
             "Unable to get URL. Please make sure it's valid and try again."
@@ -73,6 +73,21 @@ def index():
         )
         print(job.get_id())
     return render_template('index.html', errors=errors, results=results)
+
+@app.route('/results/<job_key>', methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=conn)
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
+
 
 if __name__ == '__main__':
     app.run()
