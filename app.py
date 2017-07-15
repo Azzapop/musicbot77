0c0dfb38149fb2eda06a19a2bd821c906aa415de
Azzapop <aaron.b.hook@gmail.com>
2017-07-15 19:24:52 +1000

    add angular to poll for results

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import operator
 import re
 import nltk
+import json
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from stop_words import stops
@@ -61,18 +62,21 @@ def count_and_save_words(url):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
-    results = {}
-    if request.method == 'POST':
-        # get url that the user entered
-        url = request.form['url']
-        if 'http://' not in url[:7]:
-            url = 'http://' + url
-        job = q.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
-        )
-        print(job.get_id())
-    return render_template('index.html', errors=errors, results=results)
+    return render_template('index.html')
+
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # get url
+    data = json.loads(request.data.decode())
+    url = data["url"]
+    if 'http://' not in url[:7]:
+        url = 'http://' + url
+    # start job
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+    # return created job id
+    return job.get_id()
 
 @app.route('/results/<job_key>', methods=['GET'])
 def get_results(job_key):
